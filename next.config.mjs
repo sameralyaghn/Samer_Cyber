6ae893b118116d51c54d1fbbb1ab80a17240bb8c
Samer Al yaghn <sameralyaghn9@gmail.com>
2026-03-24 23:19:40 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+/** @type {import('next').NextConfig} */ const nextConfig = 
+{ typescript: 
+  { ignoreBuildErrors: true, },
+ images: { unoptimized: true, },
+} 
 
-module.exports = nextConfig
+export default nextConfig
